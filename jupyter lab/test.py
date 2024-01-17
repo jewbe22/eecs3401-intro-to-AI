ff0c130e004d64d3f3ee557fb00208430e57588b
Geon Kim <jewbe22@gmail.com>
2024-01-16 20:38:02 -0500

    update

--- jupyter lab/test.py
+++ jupyter lab/test.py
@@ -1,1 +1,9 @@
-print("this is test")
+input = ["AAA","BBB","CCC","DDD"]
+answer = []
+for i in range(len(input)):
+    if i % 2 == 0:
+        answer.append(input[i].lower())
+    else:
+        answer.append(input[i].upper())
+
+print(answer)
